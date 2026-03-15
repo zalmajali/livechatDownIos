be3b991755ad6f902df16ed3eeef4dacbd2522b9
zeyad <112281551+zalmajali@users.noreply.github.com>
2026-03-15 12:31:04 +0300

    15

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -171,15 +171,18 @@ export class HomePage implements OnInit {
     await loading.present();
   }
     async checkLoginDataUser(){
+      alert("sdfsdsdf sdfsdf");
     this.department = await this.storage.get('department');
     this.mainUserName = await this.storage.get('mainUserName');
     this.userName = await this.storage.get('userName');
       this.firebaseMessaging.requestPermission({forceShow: false}).then(function() {
      });
+      alert("sdfsdsdf sdfsdf 789789");
      let topic = this.mainUserName+this.department;
      await this.firebaseMessaging.subscribe(topic);
      await this.firebaseMessaging.onMessage().subscribe(async (data:any)=>{
      })
+      alert("sdfsdsdf sdfsdf 3453455");
     await this.firebaseMessaging.onBackgroundMessage().subscribe(async (data:any)=>{
       if (data.chatSessionId && data.number && data.userName) {
         const contacts = await this.functionChatGetMobileInfo(data.number);
@@ -190,11 +193,10 @@ export class HomePage implements OnInit {
     let token = await this.firebaseMessaging.getToken();
     let sendValues = {'mainUser':this.mainUserName,'userName':this.userName,'dep':this.department,'token':token};
       alert(JSON.stringify(sendValues))
-      alert("sdfsdfsd");
      this.userService.loginDataUser(sendValues).then(async data=>{
-       alert("done")
+      alert(JSON.stringify(data))
     }).catch(error=>{
-       alert("sdfsdf")
+      alert(JSON.stringify(error))
       this.checkLoginDataUser();
     });
   }
